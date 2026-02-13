36d5330dca9d54828264b4c3aaffca24bf93f2a5
BodeXX <jonasalberto.dev@gmail.com>
2026-02-13 19:10:40 -0300

    feat(introducao): pratica de tipos primitivos e interpolacao

---
+++ 01-introducao/fundamentos_variaveis.dart
@@ -0,0 +1,22 @@
+// Variáveis e TIpos de Dados
+
+void main () {
+    // A variável 'name' armazena uma string (texto)
+    String name = 'Eduardo Mendes';
+
+    // A variável 'age' armazena um número inteiro
+    int age = 52;
+
+    // A variável 'height' armazena um número decimal (altura)
+    double height = 1.86;
+
+    // A variável 'isStudent' armazena um valor boolenado (verdadeiro ou falso)
+    bool isStudent = true;
+
+
+    // Exibir as informações no console
+    print('Name: $name'); // Exibe valor da variável 'name'
+    print('Age: $age'); // Exibe valor da variável 'age'
+    print('Height: $height'); // Exibe valor da variável 'height'
+    print('isStudent: $isStudent'); // Exibe valor da variável 'isStudent'
+}
