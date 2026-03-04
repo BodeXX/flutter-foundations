71fa27969fc6aaa7c93647bb10178a8e45b92742
BodeXX <jonasalberto.dev@gmail.com>
2026-03-03 23:22:43 -0300

    feat(main): integra serviço de notificação na inicialização do app

--- 04-google-cloud-messaging/gcm_study/lib/main.dart
+++ 04-google-cloud-messaging/gcm_study/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:gcm_study/firebase_options.dart';
+import 'package:gcm_study/services/push_notification_service.dart';
 
 void main() async {
   // 1. Garante a comunicação com o sistema nativo antes do Firebase
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await PushNotificationService().initialize();
 
   runApp(const MyApp());
 }
